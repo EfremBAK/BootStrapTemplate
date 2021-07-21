72968c84dd1d20abf4cba38c453c48dc9548790c
Efrem Abrehe <efrem.berhane2009@gmail.com>
2021-07-21 09:46:30 -0500

    Complete server side

--- BookApplication/BookApplication/src/main/java/books/JmsSender.java
+++ BookApplication/BookApplication/src/main/java/books/JmsSender.java
@@ -2,11 +2,15 @@ package books;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 @Component
-public class JmsSender {
+@EnableJms
+public class JmsSender  {
     @Autowired
     JmsTemplate jmsTemplate;
 
